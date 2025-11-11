c8721778122871425f2f4c890a2d909d4c629f29
DMITRIY SPIRIKHIN <117768476+DmitriySpirihin@users.noreply.github.com>
2025-11-11 08:37:52 +0300

    Updates

--- assets/Done_Icon-Vq4AKoy9.js
+++
@@ -1,1 +0,0 @@
-const t="https://dmitriyspirihin.github.io/UltyMyLife_Bot/assets/Done_Icon-CclnR7xu.png";export{t as D};
--- assets/Main_Light-CKjTrFHl.js
+++
@@ -1,1 +0,0 @@
-const i="https://dmitriyspirihin.github.io/UltyMyLife_Bot/assets/Main_Dark-BttpH-e_.png",t="https://dmitriyspirihin.github.io/UltyMyLife_Bot/assets/Main_Light-CpFVc3uH.png";export{i as M,t as a};
