47f0c4411648b9a9809f0d49ad3cdd8a6212e01a
DMITRIY SPIRIKHIN <117768476+DmitriySpirihin@users.noreply.github.com>
2025-12-06 17:28:14 +0300

    Updates

--- assets/MainMenu-ECLT0KzG.js
+++ assets/MainMenu-ECLT0KzG.js
@@ -1,4 +1,4 @@
-import{R as l,A,t as F,l as W,f as P,o as U,p as $,j as t,N as B,C as I,q as O}from"./index-bp4CYSA0.js";/*! Grained.js 
+import{R as l,A,t as F,l as W,f as P,o as U,p as $,j as t,N as B,C as I,q as O}from"./index-BhhhmDGq.js";/*! Grained.js 
 * Author : Sarath Saleem  - https://github.com/sarathsaleem 
 * MIT license: http://opensource.org/licenses/MIT 
 * GitHub : https://github.com/sarathsaleem/grained 
