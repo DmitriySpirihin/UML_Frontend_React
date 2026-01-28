78f1e25fe72c009ade2a3317a141f76666c8f40d
DMITRIY SPIRIKHIN <117768476+DmitriySpirihin@users.noreply.github.com>
2026-01-28 16:12:51 +0300

    Commit-81.3

--- src/main.jsx
+++ src/main.jsx
@@ -17,6 +17,7 @@ createRoot(document.getElementById('root')).render(
         manifestUrl={manifestUrl}
         walletsListConfiguration={{
             includeWallets: [],
+            walletsListSource: customWalletsList,
         }}
         actionsConfiguration={{
             twaReturnUrl: 'https://t.me/UltyMyLife_bot/umlminiapp'
