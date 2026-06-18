cf68eb57f885d01a6737ab7451101964493fdb57
demianavol <yaminaasfak@gmail.com>
2026-06-18 14:08:04 +0300

    Updates

--- assets/MainMenuRedesign-Bn3RYVCd.js
+++ assets/MainMenuRedesign-Bn3RYVCd.js
@@ -1,5 +1,5 @@
 const target = new URL('/UML_Frontend_React/', window.location.origin);
 target.searchParams.set('v', 'stable-20260618-1425');
-target.searchParams.set('cache_rescue', 'MainMenuRedesign-BIAjkQLg');
+target.searchParams.set('cache_rescue', 'MainMenuRedesign-Bn3RYVCd');
 window.location.replace(target.toString());
 export default {};
--- assets/TrainingCardioBus-jZnhtksh.js
+++
@@ -1,1 +0,0 @@
-import{B as e}from"./index-ByTicus3.js";const o=new e("RUNNING"),a=new e({mode:"new",dayKey:"",dInd:null});export{o as c,a as t};
